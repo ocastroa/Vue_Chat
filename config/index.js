400205e7bfd4e0947f5eb3e7285dbea987ebb07b
Oscar Castro <oscar.castro@pubnubs-MacBook-Air.local>
2019-05-13 18:27:23 -0700

    Changed index.js file

--- config/index.js
+++ config/index.js
@@ -42,8 +42,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'Vue_Chat/dist/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: 'Vue_Chat/dist/',
 
     /**
      * Source Maps
